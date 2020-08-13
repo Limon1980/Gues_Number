b16e1fad786009dac37c52fa657e8e2daa9a2139
Limon1980 <tarkalmob@yandex.ru>
2020-08-13 19:59:22 +1000

    доп. задание без замыканий с рекурсией

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,16 @@ let isNumber = function (n) {
 
 let rand = Math.ceil(Math.random() * 100);
 let number;
-let n = 10; // колличество попыток
+let n = 5; // колличество попыток
+let game;
+const reload = function (game) {
+  if (game) {
+    console.log(game);
+    window.location.reload();
+  } else {
+    alert('Спасибо за игру, Вы молодец!');
+  }
+}
 
 
 console.log(rand);
@@ -17,26 +26,28 @@ console.log(rand);
 function whoNumber() {
 
 
+  number = parseFloat(prompt('Угадай число от 1 до 100'));
+  if (!isNumber(number)) {
+    alert('Введите число');
+    whoNumber();
+  } else {
+    n--;
+  }
 
-  do {
-
-    number = parseFloat(prompt('Угадай число от 1 до 100'));
-    if (!isNumber(number)) {
-      alert('Введите число');
-    } else {
-      n--;
+  if (n > 0) {
+    if (number === rand) {
+      game = confirm('Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? ');
+      reload(game);
+    } else if (number < rand) {
+      alert('Загадонное число больше! Осталось ' + n + ' попыток');
+      whoNumber();
+    } else if (number > rand) {
+      alert('Загаданное число меньше! Осталось ' + n + ' попыток');
+      whoNumber();
     }
-
-  } while (!isNumber(number));
-
-  if (number === rand) {
-    alert('Угадал! Число ' + rand);
-  } else if (number < rand) {
-    alert('Загадонное число больше! Осталось ' + n + 'попыток');
-    whoNumber();
-  } else if (number > rand) {
-    alert('Загаданное число меньше! Осталось ' + n + 'попыток');
-    whoNumber();
+  } else {
+    game = confirm('Попытки закончились, хотите сыграть еще?');
+    reload(game);
   }
 }
 
