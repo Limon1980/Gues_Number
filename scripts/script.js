e72c39276f55c512fad173bbab51651eea54613a
Limon1980 <tarkalmob@yandex.ru>
2020-08-14 09:57:47 +1000

    Доп. задание угадай число выполнил, рекурсия и замыкания

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,6 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-
-// let rand = Math.ceil(Math.random() * 100);
-// let number;
-
-let game;
-
 const reload = function (game) {
   if (game) {
     console.log(game);
@@ -20,58 +14,28 @@ const reload = function (game) {
   }
 };
 
-
-// console.log(rand);
-
-
-// function whoNumber() {
-
-//   number = parseFloat(prompt('Угадай число от 1 до 100'));
-//   if (!isNumber(number)) {
-//     alert('Введите число');
-//     whoNumber();
-//   } else {
-//     n--;
-//   }
-
-//   if (n > 0) {
-//     if (number === rand) {
-//       game = confirm('Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? ');
-//       reload(game);
-//     } else if (number < rand) {
-//       alert('Загадонное число больше! Осталось ' + n + ' попыток');
-//       whoNumber();
-//     } else if (number > rand) {
-//       alert('Загаданное число меньше! Осталось ' + n + ' попыток');
-//       whoNumber();
-//     }
-//   } else {
-//     game = confirm('Попытки закончились, хотите сыграть еще?');
-//     reload(game);
-//   }
-// }
-
-// whoNumber();
-// console.dir(whoNumber);
-
-
 function whoNumber() {
-
   let rand = Math.ceil(Math.random() * 100);
   let n = 5; // колличество попыток
+  let game;
   console.log(rand);
 
   function quest(number = parseFloat(prompt('Угадай число от 1 до 100'))) {
     if (n > 1) {
       if (!isNumber(number)) {
-        alert('Введите число');
+        game = confirm('Введите число. Хотите продолжить?');
+        if (!game) {
+          return alert('Игра окончена!');
+        }
         quest();
       } else {
         n--;
       }
 
       if (number === rand) {
-        game = confirm('Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? ');
+        game = confirm(
+          'Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? '
+        );
         reload(game);
       } else if (number < rand) {
         alert('Загадонное число больше! Осталось ' + n + ' попыток');
