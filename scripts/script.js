871829f994f0b7f49f59664459c557c7b83b7ba1
Limon1980 <tarkalmob@yandex.ru>
2020-08-13 17:55:20 +1000

    угадай число основное задание

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// проверка на число
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+let rand = Math.ceil(Math.random() * 100);
+let number;
+
+
+
+console.log(rand);
+
+
+function whoNumber() {
+
+  do {
+
+    number = parseFloat(prompt('Угадай число от 1 до 100'));
+    if (!isNumber(number)) {
+      alert('Это не число!');
+    }
+
+  } while (!isNumber(number));
+
+  if (number === rand) {
+    alert('Угадал! Число ' + rand);
+  } else if (number < rand) {
+    alert('Загадонное число больше!');
+    whoNumber();
+  } else if (number > rand) {
+    alert('Загаданное число меньше!');
+    whoNumber();
+  }
+}
+
+whoNumber();
