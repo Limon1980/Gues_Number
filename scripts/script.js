394ce9bbeafc310509e53948aaab6f6e23226756
Limon1980 <tarkalmob@yandex.ru>
2020-08-14 10:12:54 +1000

    Угадай число основное задание, рекурсия и замыкание

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+let game;
 const reload = function (game) {
   if (game) {
     console.log(game);
@@ -16,37 +17,29 @@ const reload = function (game) {
 
 function whoNumber() {
   let rand = Math.ceil(Math.random() * 100);
-  let n = 5; // колличество попыток
-  let game;
+
   console.log(rand);
 
   function quest(number = parseFloat(prompt('Угадай число от 1 до 100'))) {
-    if (n > 1) {
-      if (!isNumber(number)) {
-        game = confirm('Введите число. Хотите продолжить?');
-        if (!game) {
-          return alert('Игра окончена!');
-        }
-        quest();
-      } else {
-        n--;
+    if (!isNumber(number)) {
+      game = confirm('Введите число. Хотите продолжить?');
+      if (!game) {
+        return alert('Игра окончена!');
       }
+      quest();
+    }
 
-      if (number === rand) {
-        game = confirm(
-          'Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? '
-        );
-        reload(game);
-      } else if (number < rand) {
-        alert('Загадонное число больше! Осталось ' + n + ' попыток');
-        quest();
-      } else if (number > rand) {
-        alert('Загаданное число меньше! Осталось ' + n + ' попыток');
-        quest();
-      }
-    } else {
-      game = confirm('Попытки закончились, хотите сыграть еще?');
+    if (number === rand) {
+      game = confirm(
+        'Поздравляю, Вы угадали!!! Число ' + rand + ' Хотели бы сыграть еще? '
+      );
       reload(game);
+    } else if (number < rand) {
+      alert('Загадонное число больше!');
+      quest();
+    } else if (number > rand) {
+      alert('Загаданное число меньше!');
+      quest();
     }
   }
   console.dir(quest);
